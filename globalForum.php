e359214bd9ab0dfca636be5cd2e18404069df77f
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2022-05-27 16:00:40 +0530

    Update globalForum.php

--- globalForum.php
+++ globalForum.php
@@ -14,6 +14,7 @@
         <link type="text/css" rel="stylesheet" href="css/material.css">
         <link type="text/css" rel="stylesheet" href="fonts/font.css">
         <link rel="icon" href="images/icon1.png" >
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <!-- Sripts -->
         <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
@@ -75,6 +76,11 @@
             <h4>
                 <a id="title-head" href="categories.php">Global Forum</a>
             </h4>
+			<div id="Buttons">
+				<button type="button" class="btn btn-primary">Start New Thread</button>
+				<button type="button" class="btn btn-primary">Latest</button>
+				<button type="button" class="btn btn-primary">Search by Tags</button>
+			</div>
             <div id="box0">
                 <center>
                     <a id="ada" href="#box1">
@@ -201,7 +207,7 @@
             &copy; 2017 &bull; Interrogate Inc.
         </div>
         
-        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
     
 </html>
