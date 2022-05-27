daccc8ce7601f54c0a4835f8cc7b706f2dcadfe6
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2022-05-27 16:44:35 +0530

    Update globalForum.php

--- globalForum.php
+++ globalForum.php
@@ -80,30 +80,50 @@
 				<button type="button" class="btn btn-primary">Start New Thread</button>
 				<button type="button" class="btn btn-primary">Latest</button>
 				<button type="button" class="btn btn-primary">Search by Tags</button>
-			</div>
-            <div>
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="card">
-						<div class="card-body">
-							<h5 class="card-title">Thread - 1</h5>
-							<p class="card-text">Tags + First Post</p>
-							<a href="#" class="btn btn-primary">Discuss</a>
-						</div>
-						</div>
-					</div>
-				<div class="col-sm-6">
-					<div class="card">
+			</div>	
+				<center>
+				<div class="card text-bg-light mb-3" style="max-width: 97%;">
+					<div class="card-header">Thread 1</div>
 					<div class="card-body">
-						<h5 class="card-title">Thread - 2</h5>
-						<p class="card-text">Tags + First Post</p>
-						<a href="#" class="btn btn-primary">Discuss</a>
+					<h5 class="card-title">Light card title</h5>
+					<p class="card-text">Tags + Contributors</p>
+					<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Discuss</button>
+					</div>
+				</div>
+				<div class="card text-bg-light mb-3" style="max-width: 97%;">
+					<div class="card-header">Thread 2</div>
+					<div class="card-body">
+					<h5 class="card-title">LCAS</h5>
+					<p class="card-text">Tags + Contributors</p>
+					<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Discuss</button>
+					</div>
+				</div>
+				</center>
+				
+				<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">New message</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						Render all the discussion threads and posts
+						<form>
+						<div class="mb-3">
+							<label for="message-text" class="col-form-label">Message:</label>
+							<textarea class="form-control" id="message-text"></textarea>
+						</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Send message</button>
 					</div>
 					</div>
 				</div>
 				</div>
-			</div>
-            </div>
+				</div>
             <div class="pop" id="tb">
                 <center><h1><b style="font-size: 1.5em; margin: -60px auto 10px; display: block;">:)</b>Thank You For Your Answer.</h1></center>
             </div>
